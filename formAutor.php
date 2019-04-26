86e30a768487113eae21745e5922fe3dead6e40e
Otto Freitas <ottofq@gmail.com>
2019-04-26 15:49:50 -0300

    Inclusão de css

--- formAutor.php
+++ formAutor.php
@@ -4,17 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css"/>
     <title>Cadastro Autor</title>
 </head>
 <body>
-    <div>
+    <div class="container">
         <form action="controlers/controlerAutor.php" method="POST">
-        Nome:<input type="text" name="txtNomeAutor"><br><br>
-        Email:<input type="text" name="txtEmailAutor"><br><br>
-        Data de Nascimento<input type="text" name="txtDataNascAutor"><br><br>
-        <input type="submit" value="Enviar">
+            <div class="form-group">
+                <label>Nome</label>
+                <input type="text" class="form-control" name="txtNomeAutor">
+            </div>
 
-        <input type="hidden" name="opcao" value="1">
+            <div class="form-group">
+                <label>Email</label>
+                <input type="email" class="form-control" name="txtEmailAutor">
+            </div>
+
+            <div class="form-group">
+                <label>Data de Nascimento</label>
+                <input type="date" class="form-control" name="txtDataNascAutor">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+            <input type="hidden" name="opcao" value="1">
         </form>
     </div>
 </body>
